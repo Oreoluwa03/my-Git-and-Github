5c01f424d4475113a4c697e8c48ca6c970441e9b
Oreoluwa03 <107117755+Oreoluwa03@users.noreply.github.com>
2022-06-29 20:02:13 +0100

    #i added my  new project

---
+++ myProject.py
@@ -0,0 +1,13 @@
+print("ALgorithm")
+#first the project is multipllying
+#create a list
+#let them multiply
+
+def multiplyList():
+    num = range(10)
+    multi = 0
+    chy = num * multi
+    for i in num:
+        print(i)
+        print(chy)
+        
