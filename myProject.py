f6f224173b3aee405cb041029b687e3be7139a35
Oreoluwa03 <107117755+Oreoluwa03@users.noreply.github.com>
2022-07-19 10:37:57 +0100

    i have made changes to it

--- myProject.py
+++ myProject.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print("Good day Bro Olamide this is a code that accepts a list and multiplies it sir ")
   
 
@@ -60,52 +59,4 @@ for i in range(1, 100 + 1):
     if getPrimenums(i):
         print(i, end=" ")
 
-print("Good day Bro Olamide this is a code that accepts a list and multiplies it sir ")
-def multiplyAll(input):
-    sum =  1
-    for nums in input:
-        print(nums)
-        sum = sum * nums
-        print(sum)
-multiplyAll()        
-
-print("this is a code that prints all primenumbers")
-def printPrimeNums():
-    nums = range(101)
-    for n in nums:
-        if n%2 == 0:
-           print(n)
-printPrimeNums()           
-print("this is a code that gets the factorial ")           
-def factorial(info):
-    info = range(1,info + 1)
-    multiply = 1
-    for info in info:
-        multiply = multiply * info
-        print(info)
-        print(multiply)
-factorial()
-
-def takesInput():
-    pull = input("enter: ")
-    print(pull)
-    return pull
-
-takesInput()   
-
-
-def myfunc(name):
-    counter = name
-    print(counter.strip(name))
-
-    figures = {}
-    for count in counter:
-        if count in figures:
-            figures[count] += 1
-        else:
-            figures[count] = 1
-    print(figures) 
-myfunc()
-
-print("Thank you")
-
+ 
